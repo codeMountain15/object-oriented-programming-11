eb3ad7f6bc341843fea5f72592ad06ce08baf663
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-22 23:36:59 +0200

    Create stream06.cpp

---
+++ source_code/stream06.cpp
@@ -0,0 +1,43 @@
+// stream06.cpp 
+// an example of the fstream class implementation
+
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	// create the fstream class object
+	fstream obj;
+
+	string temp = "";
+
+	// obj.open("donkey.txt", ios::in|ios::out|ios::app)
+	// ios::trunc mode deletes all the file content before opening it
+	
+	obj.open("donkey.txt", ios::app);
+	cout << "Type a message to the donkey!\n";
+	cin >> temp;
+
+	// insert to file loop
+	while ((temp != "0") && obj.is_open()) {
+		cout << "Type a message to the donkey!\n";
+		obj << temp;
+		cin >> temp;
+	}
+	obj.close();
+
+
+	obj.open("donkey.txt", ios::in);
+
+	// read from file loop
+	while (obj >> temp) {
+
+		cout << temp << endl;
+	}
+	obj.close();
+
+	return 0;
+}
